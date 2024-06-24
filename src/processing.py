05685eaa9f9e917698f4a65391713193200db918
Andrey Osipov <bezy4uy@gmail.com>
2024-06-24 21:02:49 +0300

    homework corrections 10.1

--- src/processing.py
+++ src/processing.py
@@ -3,9 +3,9 @@ def filter_by_state(my_list: list[dict[str, str]], state_value: str = 'EXECUTED'
     return [elem for elem in my_list if elem["state"] == state_value]
 
 
-def sort_by_date(my_list: list[dict[str, str]], sort_order: bool = True) -> list[dict[str, str]]:
+def sort_by_date(my_list: list[dict[str, str]], sort_order: bool = False) -> list[dict[str, str]]:
     """Сортирует список словарей по ключу 'date'"""
-    return sorted(my_list, key=lambda x: x['date'], reverse=sort_order)
+    return sorted(my_list, key=lambda transaction_data_dict: transaction_data_dict['date'], reverse=sort_order)
 
 
 # newbie_list = [
