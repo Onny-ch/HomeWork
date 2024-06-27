e209bab7b1466204c81857dc0d37c8d8b8793875
Andrey Osipov <bezy4uy@gmail.com>
2024-06-27 20:24:04 +0300

    cleaning up comments

--- src/processing.py
+++ src/processing.py
@@ -6,13 +6,3 @@ def filter_by_state(my_list: list[dict[str, str]], state_value: str = 'EXECUTED'
 def sort_by_date(my_list: list[dict[str, str]], sort_order: bool = False) -> list[dict[str, str]]:
     """Сортирует список словарей по ключу 'date'"""
     return sorted(my_list, key=lambda transaction_data_dict: transaction_data_dict['date'], reverse=sort_order)
-
-
-# newbie_list = [
-#     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-# ]
-# print(filter_by_state(newbie_list))
-# print(sort_by_date(newbie_list))
