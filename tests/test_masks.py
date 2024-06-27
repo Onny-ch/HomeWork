ed82053d247b95d973763e18955e06a4f95fa197
Andrey Osipov <bezy4uy@gmail.com>
2024-06-27 20:27:13 +0300

    init test_masks and developed tests for module masks

---
+++ tests/test_masks.py
@@ -0,0 +1,12 @@
+from src.masks import get_mask_card_number, get_mask_account
+import pytest
+
+
+def test_get_mask_card_number(card_number_strings):
+    for elem in card_number_strings:
+        assert get_mask_card_number(elem) == f"{elem[:4]} {elem[4:6]}** **** {elem[-4:]}"
+
+
+def test_get_mask_account(accounts_strings):
+    for elem in accounts_strings:
+        assert get_mask_account(elem) == f"**{elem[-4:]}"
