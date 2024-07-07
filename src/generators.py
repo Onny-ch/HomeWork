5a932d56557985e2d28e0cea646bbcada4e9a3b7
Andrey Osipov <bezy4uy@gmail.com>
2024-07-08 00:12:52 +0300

    added docstrings for generators/ funcs and also some minor fixes

--- src/generators.py
+++ src/generators.py
@@ -1,14 +1,20 @@
 def filter_by_currency(my_list, my_code="USD"):
+    """
+    Функция, возвращающая итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной
+    """
     for x in (dict_elem for dict_elem in my_list if dict_elem["operationAmount"]["currency"]["code"] == my_code):
         yield x
 
 
 def transaction_descriptions(my_list):
+    """Функция, анализирующая информацию о транзакциях и возвращающая описание каждой операции по очереди"""
     for x in (dict_elem["description"] for dict_elem in my_list):
         yield x
 
 
 def card_number_generator(start, stop):
+    """Функция, генерирующая номер банковской карты в соответствии с начальным и конечным значением диапазона"""
     count, card_num = start, start
     while count <= stop:
         cnd = "".join("0" for _ in range(16 - len(str(card_num)))) + str(card_num)
--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filter_by_state(my_list: list[dict[str, str]], state_value: str = 'EXECUTED') -> list[dict[str, str]]:
+def filter_by_state(my_list: list[dict[str, str]], state_value: str = "EXECUTED") -> list[dict[str, str]]:
     """Фильтрует список словарей по ключу 'state'"""
     return [elem for elem in my_list if elem["state"] == state_value]
 
 
 def sort_by_date(my_list: list[dict[str, str]], sort_order: bool = False) -> list[dict[str, str]]:
     """Сортирует список словарей по ключу 'date'"""
-    return sorted(my_list, key=lambda transaction_data_dict: transaction_data_dict['date'], reverse=sort_order)
+    return sorted(my_list, key=lambda transaction_data_dict: transaction_data_dict["date"], reverse=sort_order)
