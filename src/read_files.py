22608e8e237c581adb46a104e02a52536f02f6ed
Andrey Osipov <bezy4uy@gmail.com>
2024-09-24 13:35:41 +0300

    black changes

--- src/read_files.py
+++ src/read_files.py
@@ -1,19 +1,17 @@
-from typing import Any
-
 import pandas as pd
 
 
-def read_csv(csv_file: Any) -> Any:
+def read_csv(csv_file: str) -> list[dict]:
     """Функция считывания финансовых операций из CSV файла"""
     df = pd.read_csv(csv_file, delimiter=";")
 
-    transactions_dict = df.head().to_json(orient="records", indent=4)
+    transactions_dict = df.to_dict(orient="records")
     return transactions_dict
 
 
-def read_xls(xsl_file: Any) -> Any:
+def read_xls(xsl_file: str) -> list[dict]:
     """Функция считывания финансовых операций из Excel файла"""
     df = pd.read_excel(xsl_file)
 
-    transactions_dict = df.to_json(orient="records", indent=4)
+    transactions_dict = df.to_dict(orient="records")
     return transactions_dict
