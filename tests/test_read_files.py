de0c502dd333ee404d36499295b26507917a6d38
Andrey Osipov <bezy4uy@gmail.com>
2024-09-26 15:55:01 +0300

    pytest tests fixes

--- tests/test_read_files.py
+++ tests/test_read_files.py
@@ -1,3 +1,4 @@
+import json
 from unittest.mock import Mock, patch
 
 import pandas as pd
@@ -8,15 +9,18 @@ from src.read_files import read_csv, read_xls
 @patch("pandas.read_csv")
 def test_read_csv(mock_read_csv):
     mock_read_csv.return_value = pd.DataFrame({"column1": [1, 2], "column2": ["a", "b"]})
+
     expected_json = mock_read_csv.return_value.to_json(orient="records", indent=4)
-    assert read_csv("data\\transactions.csv") == expected_json
+    test_results = json.loads(expected_json)
+
+    assert read_csv("data\\transactions.csv") == test_results
 
 
 def test_read_xls():
     mock_read_xls = pd.DataFrame({"column1": [1, 2], "column2": ["a", "b"]})
     expected_json = mock_read_xls.to_json(orient="records", indent=4)
+    test_result = json.loads(expected_json)
 
-    mock_random = Mock(return_value=mock_read_xls)
-    pd.read_excel = mock_random
+    pd.read_excel = Mock(return_value=mock_read_xls)
 
-    assert read_xls("data\\transactions_excel.xlsx") == expected_json
+    assert read_xls("data\\transactions_excel.xlsx") == test_result
